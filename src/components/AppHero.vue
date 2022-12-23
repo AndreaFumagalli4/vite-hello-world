8c7173e471981f4851734f980d84f4f74dab0442
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-12-23 16:58:39 +0100

    add hero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+        <img src="../assets/merry-xmas.webp" alt="Merry xmas image">
+    </div>
+</template>
+
+<style scoped>
+div{
+    width: 80%;
+    object-fit: cover;
+    margin: 0 auto;
+}
+img {
+    width: 100%;
+}
+</style>
